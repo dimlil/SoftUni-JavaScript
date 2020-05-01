2ca4eff9f06d52b02694ff5058b82e560a97aae8
dimlil <d.v.vassilev@gmail.com>
2020-05-01 22:42:48 +0300

    Deleted folder

--- Javascript Tech Fundamentals Softuni/Exam tasks/Mid Exam 10.03.2019/01.js
+++
@@ -1,8 +0,0 @@
-function f(inputArr) {
-    let days=inputArr.shift();
-    let budget=inputArr.shift();
-    let groupOfPeople=inputArr.shift();
-    let fuelPerKilometer=inputArr.shift();
-    let food=inputArr.shift();
-    let priceFor1nightFor1Person=inputArr.shift();
-}
