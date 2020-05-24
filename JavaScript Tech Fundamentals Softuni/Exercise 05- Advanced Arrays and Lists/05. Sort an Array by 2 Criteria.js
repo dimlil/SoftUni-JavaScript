0b0da9b400dc830608e26ac74bdeec6cd825c35a
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-05-25 02:02:43 +0300

    old homework

---
+++ JavaScript Tech Fundamentals Softuni/Exercise 05- Advanced Arrays and Lists/05. Sort an Array by 2 Criteria.js
@@ -0,0 +1,14 @@
+function solve(inputArr) {
+    inputArr.sort((a,b)=>a.length-b.length);
+    inputArr.sort((a,b)=>a-b);
+    inputArr.forEach(element => {
+        console.log(element);
+    });
+}
+solve(["alpha", "beta", "gamma"]);
+//solve([ "Deny","test", "omen","Default"]); 
+// Deny
+// omen
+// test
+// Default
+//https://judge.softuni.bg/Contests/Compete/Index/1299#4
