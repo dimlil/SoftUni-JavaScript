53faae3b13fb56ca61f7551972b2dd4dd717dd49
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-06-06 17:10:49 +0300

    Added code

--- JavaScript Web/Lecture 03-ExpressJS and Templating/index.js
+++ JavaScript Web/Lecture 03-ExpressJS and Templating/index.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const homePageRouter=require('./routes/home-page')
 
-app.get('/', (req, res) => {
-    res.send('Welcome to our application');
-});
+app.use('/',homePageRouter)
+
+app.get('/something', (req, res, next) => {
+    //sumolating going to db logic
+    req.numbers=[1,2,3];
+    next()
+}, (req, res) => {
+    res.send(`numbers: ${req.numbers}`);
+})
 app.get('/search', (req, res) => {
     res.send('Search Page');
 });
 app.post('/users', (req, res) => {
     res.send('Users:[1,2,3,4]');
 });
-
+app.get('/profile/:user/:id', (req, res) => {
+    const user=req.params.user;
+    const id=req.params.id;
+    res.send(`user: ${user}, id: ${id}`);
+});
+app.all('*',(req,res)=>{
+    res.send("PAGE NOT FOUND");
+});
 app.listen(port, (err) => {
     if (err) {
         console.log('ERROR: ', err);
