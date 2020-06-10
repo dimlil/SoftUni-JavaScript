23a67a86076f799780d5338e65c336f2c871a489
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-06-10 18:26:24 +0300

    Added static files

--- JavaScript Web/Lecture 03-ExpressJS and Templating/index.js
+++ JavaScript Web/Lecture 03-ExpressJS and Templating/index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const homePageRouter=require('./routes/home-page');
+const homePageRouter = require('./routes/home-page');
+const handlebars = require('express-handlebars')
 
-app.use('/',homePageRouter)
+app.engine('.hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
+app.use(express.static('public'));
+app.use('/static', express.static(__dirname + '/public/static.html'));
+
+app.use('/', homePageRouter);
 
 app.get('/something', (req, res, next) => {
     //sumolating going to db logic
-    req.numbers=[1,2,3];
-    next()
+    req.numbers = [1, 2, 3];
+    next();
 }, (req, res) => {
     res.send(`numbers: ${req.numbers}`);
-})
+});
 app.get('/search', (req, res) => {
     res.send('Search Page');
 });
@@ -19,11 +29,11 @@ app.post('/users', (req, res) => {
     res.send('Users:[1,2,3,4]');
 });
 app.get('/profile/:user/:id', (req, res) => {
-    const user=req.params.user;
-    const id=req.params.id;
+    const user = req.params.user;
+    const id = req.params.id;
     res.send(`user: ${user}, id: ${id}`);
 });
-app.all('*',(req,res)=>{
+app.all('*', (req, res) => {
     res.send("PAGE NOT FOUND");
 });
 app.listen(port, (err) => {
@@ -32,4 +42,4 @@ app.listen(port, (err) => {
         return;
     }
     console.log(`Server is up and running on port: ${port}`);
-})
+});
