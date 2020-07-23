c701b9912a54bf1f74b3966b1069fe93fa6d88e9
Dimitar Vassilev <d.v.vassilev@gmail.com>
2020-07-24 02:28:01 +0300

    Test sessions and cookies

--- JavaScript Web/Lecture 05- Session and Authentication/index.js
+++ JavaScript Web/Lecture 05- Session and Authentication/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 const app= express();
-
-app.use(cookieParser())
+app.use(cookieParser());
+app.use(session({secret:'softuni-secret-key'},{httpOnly: true},{secure:true}))
 
 app.get('/',(req,res)=>{
+    res.cookie('message','Hello auth server')
+    res.cookie('sid','c16565132316')
+
+    req.session.userId=1
+    req.session.email='dim@abv.bg'
+
     res.send({
         message: "Hello Auth Server"
+    })
+})
+app.get('/search',(req,res)=>{
+    console.log(req.cookies);
+    res.send({
+        pageMessage: 'index page',
+        ...req.cookies,
+        session: req.session
     })
 })
 
