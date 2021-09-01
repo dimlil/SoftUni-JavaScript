1daf1b4f1b007071895ce714a03e50d533cda14a
dimlil <d.v.vassilev@gmail.com>
2021-09-01 22:38:02 +0300

    modified

--- JavaScript Tech Fundamentals Softuni/Exercise 01- Basic Syntax, Conditional Statements and Loops/01. Ages.js
+++ JavaScript Tech Fundamentals Softuni/Exercise 01- Basic Syntax, Conditional Statements and Loops/01. Ages.js
@@ -1,18 +1,21 @@
 function solve(age) {
-    if(age>=0&&age<=2){
+    age = Number(age);
+    if (age >= 0 && age <= 2) {
         console.log("baby");
     }
-    if(age>=3&&age<=13){
+    if (age >= 3 && age <= 13) {
         console.log("child");
     }
-    if(age>=14&&age<=19){
+    if (age >= 14 && age <= 19) {
         console.log("teenager");
     }
-    if(age>=20&&age<=65){
+    if (age >= 20 && age <= 65) {
         console.log("adult");
     }
-    if(age>=66){
-            console.log("elder");
+    if (age >= 66) {
+        console.log("elder");
     }
-
 }
+solve(2)
+solve(64)
+solve(100)
